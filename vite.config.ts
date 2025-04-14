d5908cd1d111b2c008bff4b0cb9611a38088a4f6
Ramya-Shah <176913971+Ramya-Clg@users.noreply.github.com>
2025-04-15 00:46:15 +0530

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,12 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
+    server: {
+        host: '0.0.0.0',  // This ensures that the dev server binds to all network interfaces
+        port: 5173
+    },
+    plugins: [react()],
+    optimizeDeps: {
+        exclude: ['lucide-react'],
+    },
 });
