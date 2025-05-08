22c9e6367118cbefe5ab9e9a6ab153ccf2618f8d
Ramya-Shah <176913971+Ramya-Clg@users.noreply.github.com>
2025-05-08 11:50:20 +0530

    fix

--- server/index.js
+++ server/index.js
@@ -34,8 +34,7 @@ app.use(express.static(path.join(__dirname, '../dist')));
 mongoose
   .connect(process.env.MONGODB_URL)
   .then(() => console.log('Connected to MongoDB'))
-  .catch(err => 
-    console.error('MongoDB connection error:', err));
+  .catch(err => console.error('MongoDB connection error:', err));
 
 // Game state
 const rooms = {};
